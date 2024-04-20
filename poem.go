9835538e60eb22790f9522068f7c483ede3bfc14
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-04-20 12:40:55 -0500

    fixing newline char issue

--- poem.go
+++ poem.go
@@ -20,6 +20,6 @@ func getRandomPoem() string {
 	total_poems := len(records) - 1          // Subtracting the header
 	random_idx := rand.Intn(total_poems) + 1 // Add 1 to avoid header
 	poem := records[random_idx][1]
-	poem = strings.Replace(poem, "\n", "\r\n", -1)
+	poem = strings.Replace(poem, "\\n", "\n", -1)
 	return poem
 }
